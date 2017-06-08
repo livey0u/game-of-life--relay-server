2df0ff37946e2a024d590e054bd4ac5e1dfd372b
மதி <tamilvendhan@TMP.local>
2017-06-08 08:45:50 +0530

    Added logs. Refactored init flow. Fixed config files.

--- index.js
+++ index.js
@@ -5,20 +5,23 @@ const WebSocket = require('ws');
 const SocketIOClient = require('socket.io-client');
 const config = require('config');
 const logger = require('logger');
-const constants = require('constants');
+const constants = require('./constants');
 
 const server = http.createServer();
 const wsServerConfig = {port: process.env.PORT || config.server.port, host: config.server.host};
 const relayServer = new WebSocket.Server({server: server});
-const gameOfLifeClientURL = config.world.url;
-const gameOfLifeClient = new SocketIOClient(gameOfLifeClientURL);
+const gameOfLifeWorldURL = config.world.url;
+const gameOfLifeWorldClient = new SocketIOClient(gameOfLifeWorldURL);
+
+relayServer.on('error', function onError(error) {
+  logger.info('Relay server error', error);
+});
 
 relayServer.on('connection', function onConnection(ws, req) {
 
 	let address = req.headers['x-forwarded-for'] ? req.headers['x-forwarded-for'] : req.connection.remoteAddress;
-
-	gameOfLifeClient.emit(constants.NEW_CLIENT, {address: address}, function fn(error, data) {
-    logger.info('Web Client Connected', {event: data.event});
+  
+	gameOfLifeWorldClient.emit(constants.NEW_CLIENT, {address: address}, function onResponse(error, data) {
 		send(ws, data);
 	});
 
@@ -32,10 +35,9 @@ relayServer.on('connection', function onConnection(ws, req) {
     catch(ex) {
     	return logger.error('message error', {message: message});
     }
-    logger.info('Incoming Message', payload);
-    return gameOfLifeClient.emit(payload.event, payload.data, function fn(error, data) {
+
+    return gameOfLifeWorldClient.emit(payload.event, payload.data, function onResponse(error, data) {
     	if(error) {
-    		logger.error(message, error);
     		return sendError(ws, error);
     	}
     	send(ws, data);
@@ -51,7 +53,7 @@ relayServer.on('connection', function onConnection(ws, req) {
     logger.info('Web Client error', error);
   });
 
-  logger.info('Web Client Connected', {address: address});
+  logger.info('Web Client Connected. Relaying data to world server.', {event: constants.NEW_CLIENT, data: {address: address}});
 
 });
 
@@ -65,19 +67,23 @@ function send(client, data) {
   }
 }
 
-gameOfLifeClient.on('message', function onMessage(data) {
+gameOfLifeWorldClient.on('message', function onMessage(data) {
 	relayServer.clients.forEach(function each(client) {
     send(client, data);
   });
 });
 
-gameOfLifeClient.on('error', function onError(error) {
+gameOfLifeWorldClient.on('error', function onError(error) {
 	logger.error('World server client connection error', error);
 });
 
-server.listen(wsServerConfig.port, wsServerConfig.host, function onListen(error) {
-  if(error) {
-    return logger.error('Unable to listen to server', error);
-  } 
-  logger.info('GameOfLife Relay Server running on ' + wsServerConfig.port);
+logger.info('Connecting to game of life world server ', gameOfLifeWorldURL);
+gameOfLifeWorldClient.on('connect', function onConnect(data) {
+  logger.info('Connected to game of life world server ', gameOfLifeWorldURL);
+  server.listen(wsServerConfig.port, wsServerConfig.host, function onListen(error) {
+    if(error) {
+      return logger.error('Unable to listen to server', error);
+    } 
+    logger.info(`GameOfLife Relay Server running on ws://${wsServerConfig.host}:${wsServerConfig.port}`);
+  });
 });
